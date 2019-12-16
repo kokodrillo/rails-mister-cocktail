af6dcaea0bd1581950e9c7785509eb697eefd45c
Maja Polanc <mayainstudio@gmail.com>
2019-12-16 19:43:03 +0100

    changed input of description to text-area, added style

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,4 +2,5 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   validates :name, uniqueness: true, presence: true
+  validates :description, length: { maximum: 200 }, on: :create, uniqueness: true, allow_nil: false, presence: true
 end
