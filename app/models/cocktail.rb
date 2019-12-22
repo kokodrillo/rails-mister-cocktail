7f0285dadaf23c8468812ac69a3ea90c79a35f17
Maja Polanc <mayainstudio@gmail.com>
2019-12-22 18:19:10 +0100

    adding image model with active storage, design of cards

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,7 @@
 class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+  has_one_attached :image
   validates :name, uniqueness: true, presence: true
   validates :description, length: { maximum: 200 }, on: :create, uniqueness: true, allow_nil: false, presence: true
 end
